158630dcfacdbba1579d47264344fc15920b2309
Iryna Hrynko <ir.hrynko@gmail.com>
2022-12-22 12:58:56 +0100

    Update index.js

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,4 +1,13 @@
-export default function Burgers() {
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:5000/items");
+  const data = await response.json();
+
+  return {
+    props: { burgers: data },
+  };
+};
+
+export default function Burgers({ burgers }) {
   return (
     <div>
       <h1>Our Burgers</h1>
